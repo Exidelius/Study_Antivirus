a4a483c67d2aea753dfe99d4f3d731524f7e0ac9
Mikhail <mir10102@mail.ru>
2023-04-28 16:41:41 +0300

    day 2 try to die

--- Antivirus/Service/Scan.cs
+++ Antivirus/Service/Scan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Antivirus.Data;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace Antivirus.Service
 {
-    internal class Scan
+    public static class Scan
     {
+        private static bool _isPause = false;
+        private static bool _isStop = false;
+
+        private static List<string> txtSign = File.ReadAllLines(ServiceInformation.DBPath).ToList();
+
+
+        public static void SetStop(bool value)
+        {
+            _isStop = value;
+        }
+
+        public static void SetPause(bool value)
+        {
+            _isStop = value;
+        }
+
+        public static List<string> ScanFolder(string path)
+        {
+            List<string> result = new List<string>();
+
+            var files = GetFiles(path);
+
+            foreach (var file in files)
+            {
+                string 
+            }
+
+            return result;
+        }
     }
 }
